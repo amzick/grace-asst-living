7b8161cff8600d1748f095fbe3a958a38c7907d4
Aaron Zick <aaron.zick@gmail.com>
2020-12-28 14:55:34 -0500

    Init and Edit Svgs

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,25 @@ import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 import { Button, Navbar, Nav, NavItem } from "react-bootstrap";
+import LogoImg from "../images/nav-logo.png"
+import LogoSymbol from "../images/logo-symbol.svg"
+import LogoText from "../images/logo-text.svg"
+import LogoTextGrace from "../images/logo-text__grace.svg"
+import LogoTextLLC from "../images/logo-text__llc.svg"
 
 const Header = ({ siteTitle }) => (
   <Navbar bg="dark" variant="dark" expand="lg">
     <Navbar.Brand as={Link} href="/">
-      {siteTitle}
+      <img
+        src={LogoSymbol}
+        // src='https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg'
+        alt='Grace Assisted Living'
+        // width="400"
+        // height="30"
+      />
+      {/* <img src={LogoText} alt='Grace Assisted Living' /> */}
+      <img src={LogoTextGrace} alt='Grace Assisted Living' />
+      <img src={LogoTextLLC} alt='Grace Assisted Living' />
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
